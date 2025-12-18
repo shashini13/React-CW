25e2216d8a98264f45f7ecacece8577d1ea145e2
Shashini <rshashini13@gmail.com>
2025-12-18 18:53:56 +0530

    Added the useState hooks and started the form structure in SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { DatePicker } from 'react-widgets/cjs';
+import 'react-widgets/styles.css';
 
+const SearchForm = () => {
+    const [type, setType] = useState("Any");
+
+    const [minPrice, setMinPrice] = useState(null);
+    const [maxPrice, setMaxPrice] = useState(null);
+
+    const [minBedrooms, setMinBedrooms] = useState(null);
+    const [maxBedrooms, setMaxBedrooms] = useState(null);
+
+    const [dateAdded, setDateAdded] = useState(null);
+    const [postcode, setPostcode] = useState("");
+
+    return (
+        <form>
+            <label>Property Type</label>
+            <Select 
+                options={[ 
+                    {value: "Any", label:"Any"}, 
+                    {value: "House", label:"House"}, 
+                    {value: "Flat", label:"Flat"}
+                ]}
+                onChange={(selected) => setType(selected.value)}
+                placeholder="Select type..."
+            />    
+        </form>
+    );
+}
 
 export default SearchForm;
