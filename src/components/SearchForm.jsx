60ee7848bb1207cde9845ffb7cbe5089c17fef8c
Shashini <rshashini13@gmail.com>
2025-12-18 19:06:05 +0530

    Added select options for min/max price/bedrooms

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Select from 'react-select/base';
 import { DatePicker } from 'react-widgets/cjs';
 import 'react-widgets/styles.css';
 
@@ -25,6 +26,54 @@ const SearchForm = () => {
                 ]}
                 onChange={(selected) => setType(selected.value)}
                 placeholder="Select type..."
+            />   
+
+            <label>Minimum Price (£)</label> 
+            <Select
+                options={[
+                    {value: 100000, label: "100 000"},
+                    {value: 200000, label: "200 000"},
+                    {value: 300000, label: "300 000"},
+                    {value: 400000, label: "400 000"},
+                ]}
+                onChange={(selected) => setMinPrice(selected.value)}
+                placeholder="Min price"
+            /> 
+
+            <label>Maximum Price (£)</label> 
+            <Select
+                options={[
+                    {value: 400000, label: "400 000"},
+                    {value: 500000, label: "500 000"},
+                    {value: 1000000, label: "1 000 000"},
+                    {value: 2000000, label: "2 000 000"},
+                ]}
+                onChange={(selected) => setMaxPrice(selected.value)}
+                placeholder="Max price"
+            /> 
+
+            <label>Minimum Bedrooms</label> 
+            <Select
+                options={[
+                    {value: 1, label: "1"},
+                    {value: 2, label: "2"},
+                    {value: 3, label: "3"},
+                    
+                ]}
+                onChange={(selected) => setMinBedrooms(selected.value)}
+                placeholder="Min Bedrooms"
+            /> 
+
+            <label>Maximum Bedrooms</label> 
+            <Select
+                options={[
+                    {value: 3, label: "3"},
+                    {value: 4, label: "4"},
+                    {value: 5, label: "5"},
+                    
+                ]}
+                onChange={(selected) => setMaxBedrooms(selected.value)}
+                placeholder="Max Bedrooms"
             />    
         </form>
     );
