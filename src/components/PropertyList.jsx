63af4bd422d4299aa89459ae222aad9d0ad69008
Shashini <rshashini13@gmail.com>
2025-12-19 10:27:26 +0530

    Added PropertyList logic

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropertyCard from './PropertyCard';
 
-const PropertyList = () => {
-    
+const PropertyList = ({results}) => {
+    if (!results || results.length === 0) {
+        return <p>No properties found.</p>
+    };
+
+    return (
+        <div>
+            {results.map((p) => (
+            <PropertyCard
+                key={p.id}
+                p = {p}
+            />
+            ))}
+        </div>
+    );
 }
 
 export default PropertyList;
