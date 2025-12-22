698b2d6c2e9a11cac0262dea8eb06294b7b239ce
Shashini <rshashini13@gmail.com>
2025-12-22 12:48:34 +0530

    Added one prop picture to test output

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,7 +1,7 @@
 const PropertyCard = ({p}) => {
     return (
         <div className="property-card">
-            <img src={p.image}></img>
+            <img src={p.picture}></img>
             <h3>{p.type} - £{p.price.toLocaleString()}</h3>
             <p>{p.bedrooms} Bedrooms</p>
             <p>Location: {p.location}</p>
