c25215395e491c3aa2d35711b2d05e58548a97dd
Shashini <rshashini13@gmail.com>
2025-12-22 16:56:35 +0530

    Created function to check if draggable & to handle drag

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,6 +1,16 @@
-const PropertyCard = ({p, onFavourite, isFavourite}) => {
+const PropertyCard = ({p, onFavourite, isFavourite=false, draggable=true}) => {
+
+    const handleDragStart = (e) => {
+        if (draggable) {
+            e.dataTransfer.setData('application/JSON', JSON.stringify(p));
+        }
+    }   
+
     return (
-        <div className="property-card">
+        <div className="property-card"
+            draggable={draggable}
+            onDragStart={handleDragStart}
+        >
             <img src={p.picture}></img>
             <h3>{p.type} - £{p.price.toLocaleString()}</h3>
             <p>{p.bedrooms} Bedrooms</p>
