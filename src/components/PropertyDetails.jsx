d49a4af6197023699395c874221d443dc478b151
Shashini <rshashini13@gmail.com>
2025-12-23 20:06:17 +0530

    Created a tabs component

--- src/components/PropertyDetails.jsx
+++ src/components/PropertyDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import ReactImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const PropertyDetails = ({allProperties}) => {
     const {id} = useParams();
@@ -19,6 +21,16 @@ const PropertyDetails = ({allProperties}) => {
     return (
         <div>
             <h2>{property.type} - £{property.price.toLocaleString()}</h2>
+            
+            <Tabs>
+                <TabList>
+                    <Tab>Images</Tab>
+                    <Tab>Description</Tab>
+                    <Tab>Floor Plan</Tab>
+                    <Tab>Map</Tab>
+                </TabList>
+            </Tabs>
+
             <ReactImageGallery items={images}/>
             <p>{property.description}</p>
             
