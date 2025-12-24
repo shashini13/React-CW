5217508f834ccf41c4181328569a97c004338703
Shashini <rshashini13@gmail.com>
2025-12-24 10:42:37 +0530

    Modified search form and added css

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -15,8 +15,6 @@ const SearchForm = ({onSearch}) => {
     const [dateAdded, setDateAdded] = useState(null);
     const [postcode, setPostcode] = useState("");
 
-    const [filters, setFilters] = useState({});
-
     const handleSubmit = (e) => {
         e.preventDefault();
         onSearch({type, minPrice, maxPrice, minBedrooms, maxBedrooms, dateAdded, postcode});
@@ -24,78 +22,95 @@ const SearchForm = ({onSearch}) => {
 
     return (
         <form className="search-form" onSubmit={handleSubmit}>
-            <label>Property Type</label>
-            <Select 
-                options={[ 
-                    {value: "Any", label:"Any"}, 
-                    {value: "House", label:"House"}, 
-                    {value: "Flat", label:"Flat"}
-                ]}
-                onChange={(selected) => setType(selected.value)}
-                placeholder=""
-            />
+            <div className="property-type-input-div">
+                <label className="search-form-label">Property Type</label>
+                <Select 
+                    options={[ 
+                        {value: "Any", label:"Any"}, 
+                        {value: "House", label:"House"}, 
+                        {value: "Flat", label:"Flat"}
+                    ]}
+                    onChange={(selected) => setType(selected.value)}
+                    placeholder=""
+                />
+            </div>
 
-            <label>Minimum Price (£)</label> 
-            <Select
-                options={[
-                    {value: 100000, label: "100 000"},
-                    {value: 200000, label: "200 000"},
-                    {value: 300000, label: "300 000"},
-                    {value: 400000, label: "400 000"},
-                ]}
-                onChange={(selected) => setMinPrice(selected.value)}
-                placeholder=""
-            /> 
+            <div className="price-bedroom-div">
+                <div>
+                    <label className="search-form-label">Minimum Price (£)</label>
+                    <Select
+                        options={[
+                            {value: 100000, label: "100 000"},
+                            {value: 200000, label: "200 000"},
+                            {value: 300000, label: "300 000"},
+                            {value: 400000, label: "400 000"},
+                        ]}
+                        onChange={(selected) => setMinPrice(selected.value)}
+                        placeholder=""
+                    /> 
+                </div>
 
-            <label>Maximum Price (£)</label> 
-            <Select
-                options={[
-                    {value: 400000, label: "400 000"},
-                    {value: 500000, label: "500 000"},
-                    {value: 1000000, label: "1 000 000"},
-                    {value: 2000000, label: "2 000 000"},
-                ]}
-                onChange={(selected) => setMaxPrice(selected.value)}
-                placeholder=""
-            /> 
+                <div>
+                    <label className="search-form-label">Maximum Price (£)</label> 
+                    <Select
+                        options={[
+                            {value: 400000, label: "400 000"},
+                            {value: 500000, label: "500 000"},
+                            {value: 1000000, label: "1 000 000"},
+                            {value: 2000000, label: "2 000 000"},
+                        ]}
+                        onChange={(selected) => setMaxPrice(selected.value)}
+                        placeholder=""
+                    /> 
+                </div>
+            
+                <div>
+                    <label className="search-form-label">Minimum Bedrooms</label> 
+                    <Select
+                        options={[
+                            {value: 1, label: "1"},
+                            {value: 2, label: "2"},
+                            {value: 3, label: "3"},
+                            
+                        ]}
+                        onChange={(selected) => setMinBedrooms(selected.value)}
+                        placeholder=""
+                    /> 
+                </div>
 
-            <label>Minimum Bedrooms</label> 
-            <Select
-                options={[
-                    {value: 1, label: "1"},
-                    {value: 2, label: "2"},
-                    {value: 3, label: "3"},
-                    
-                ]}
-                onChange={(selected) => setMinBedrooms(selected.value)}
-                placeholder=""
-            /> 
+                <div>
+                    <label className="search-form-label">Maximum Bedrooms</label> 
+                    <Select
+                        options={[
+                            {value: 3, label: "3"},
+                            {value: 4, label: "4"},
+                            {value: 5, label: "5"},
+                            
+                        ]}
+                        onChange={(selected) => setMaxBedrooms(selected.value)}
+                        placeholder=""
+                    /> 
+                </div>
+            </div>
 
-            <label>Maximum Bedrooms</label> 
-            <Select
-                options={[
-                    {value: 3, label: "3"},
-                    {value: 4, label: "4"},
-                    {value: 5, label: "5"},
-                    
-                ]}
-                onChange={(selected) => setMaxBedrooms(selected.value)}
-                placeholder=""
-            /> 
+            <div className="date-postcode-div">
+                <div>
+                    <label className="search-form-label">Date Added</label>    
+                    <DatePicker
+                        selected={dateAdded}   
+                        onChange={(date) => setDateAdded(date)}
+                    />
+                </div>    
 
-            <label>Date Added</label>    
-            <DatePicker
-                selected={dateAdded}   
-                onChange={(date) => setDateAdded(date)}
-            />    
-
-            <label>Postcode (eg: BR5)</label>
-            <input
-                type="text"
-                value={postcode}
-                onChange={(code) => setPostcode(code.target.value.toUpperCase())}
-                placeholder="Enter postcode"
-            />  
+                <div>
+                    <label className="search-form-label">Postcode (eg: BR5)</label>
+                    <input
+                        type="text"
+                        value={postcode}
+                        onChange={(code) => setPostcode(code.target.value.toUpperCase())}
+                    />  
+                </div>
+            </div>
 
             <button type="submit">Search</button>      
         </form>
