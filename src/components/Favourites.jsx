dd5debd90a9adc11fe5d0490a244b713b53b6ba4
Shashini <rshashini13@gmail.com>
2025-12-26 15:05:34 +0530

    Created relevant classes for favourites section to add css

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -24,41 +24,43 @@ const Favourites = ({favouriteProperties, removeFavourite, clearFavourites, addF
 
 
     return (
-        <div>
+        <div className="favourites">
             <h2>Favourite Properties</h2>
 
-            <div 
-                className="add-drag-drop"
-                onDrop={handleDrop}
-                onDragOver={handleDragOver}>
+            <div className="add-remove-section">
+                <div 
+                    className="add-drag-drop"
+                    onDrop={handleDrop}
+                    onDragOver={handleDragOver}>
 
-                {favouriteProperties.length > 0 && (
-                    <button onClick={clearFavourites}>Clear Favourites</button>
-                )}
+                    {favouriteProperties.length > 0 && (
+                        <button onClick={clearFavourites}>Clear Favourites</button>
+                    )}
 
-                {favouriteProperties.length === 0 ? (
-                    <p>You have no favourite properties yet</p>
-                ) : (
-                    <div>
-                        {favouriteProperties.map(p => (
-                            <PropertyCard
-                                key={p.id}
-                                p={p}
-                                onFavourite={() => removeFavourite(p.id)} 
-                                draggable={true}
-                                isFavourite={true}
-                            />
-                        ))}
-                    </div>
-                )}
-            </div>    
+                    {favouriteProperties.length === 0 ? (
+                        <p>You have no favourite properties yet</p>
+                    ) : (
+                        <div>
+                            {favouriteProperties.map(p => (
+                                <PropertyCard
+                                    key={p.id}
+                                    p={p}
+                                    onFavourite={() => removeFavourite(p.id)} 
+                                    draggable={true}
+                                    isFavourite={true}
+                                />
+                            ))}
+                        </div>
+                    )}
+                </div>    
 
-            <div 
-                className="remove-drag-drop"
-                onDrop={handleDropRemove}
-                onDragOver={handleDragOver}
-            >
-            Drag here to remove a favourite
+                <div 
+                    className="remove-drag-drop"
+                    onDrop={handleDropRemove}
+                    onDragOver={handleDragOver}
+                >
+                    Drag here to remove a favourite
+                </div>
             </div>
 
         </div>
