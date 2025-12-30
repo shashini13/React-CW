16c57ffcc69e1136df3f571a6077ddb82ccece76
Shashini <rshashini13@gmail.com>
2025-12-30 23:03:25 +0530

    Created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="header-div">
+            <img src="/images/logo.png" alt="website-logo" className="logo-img"></img>
+            <h2>Find a place you'll love to call home</h2>
+        </div>
+    );
+}
+
+export default Header;
