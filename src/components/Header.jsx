d36f3a1e74f609681ae8a4f713f0974261b6384d
Shashini <rshashini13@gmail.com>
2025-12-31 09:43:05 +0530

    Updated h2 tag in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className="header-div">
             <img src="/images/logo.png" alt="website-logo" className="logo-img"></img>
-            <h2>Find a place you'll love to call home</h2>
+            <h2>Find a place you'll love to call home !</h2>
         </div>
     );
 }
