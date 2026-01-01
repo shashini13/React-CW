131e975ea5d6b90ea0275709a33b9380faab21b9
Shashini <rshashini13@gmail.com>
2026-01-01 10:21:43 +0530

    Changed base URL for header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 const Header = () => {
+    const base = import.meta.env.BASE_URL;
     return (
         <div className="header-div">
-            <img src="/images/logo.png" alt="website-logo" className="logo-img"></img>
+            <img src={`${base}images/logo.png`} alt="website-logo" className="logo-img" />
             <h2>Find a place you'll love to call home !</h2>
         </div>
     );
