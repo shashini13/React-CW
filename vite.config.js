aa4c976254e44402ae52e3eef12a697b3131e7ab
Shashini <rshashini13@gmail.com>
2026-01-01 12:49:34 +0530

    Added vitest config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/React-CW/',
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/setupTests.js',
+  },
 })
