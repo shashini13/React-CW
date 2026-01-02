b9136066996cd5852032c328a4eaead9191e975c
Shashini <rshashini13@gmail.com>
2026-01-02 23:27:38 +0530

    Created test file to test functions in the PropertyCard component

---
+++ src/tests/PropertyCard.test.jsx
@@ -0,0 +1,5 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import PropertyCard from '../components/PropertyCard';
+import { vi, describe, it, expect } from 'vitest';
+
