fe1ea94384f4dc9cdfdeab681f1c435f1e5ac2e5
Shashini <rshashini13@gmail.com>
2026-01-02 23:42:37 +0530

    Testing code for searching using multiple filters

--- src/tests/SearchForm.test.jsx
+++ src/tests/SearchForm.test.jsx
@@ -29,4 +29,35 @@ describe('SearchForm Component', () => {
       })
     );
   });
+
+  test('filters properties using multiple filters (type + price range)', async () => {
+    const user = userEvent.setup();
+
+    const typeSelect = screen.getAllByRole('combobox')[0];
+    const minPriceSelect = screen.getAllByRole('combobox')[1];
+    const maxPriceSelect = screen.getAllByRole('combobox')[2];
+
+    fireEvent.focus(typeSelect);
+    fireEvent.change(typeSelect, { target: { value: 'House' } });
+    fireEvent.keyDown(typeSelect, { key: 'Enter', code: 'Enter' });
+
+    fireEvent.focus(minPriceSelect);
+    fireEvent.change(minPriceSelect, { target: { value: 200000 } });
+    fireEvent.keyDown(minPriceSelect, { key: 'Enter', code: 'Enter' });
+
+    fireEvent.focus(maxPriceSelect);
+    fireEvent.change(maxPriceSelect, { target: { value: 500000 } });
+    fireEvent.keyDown(maxPriceSelect, { key: 'Enter', code: 'Enter' });
+
+    const submitButton = screen.getByRole('button', { name: /search/i });
+    await user.click(submitButton);
+
+    expect(onSearchMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        type: 'House',
+        minPrice: 200000,
+        maxPrice: 500000
+      })
+    );
+  });
 });
