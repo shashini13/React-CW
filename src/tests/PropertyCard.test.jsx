14b6a804d11cdf04e2a3626bd41cbada988f8f3d
Shashini <rshashini13@gmail.com>
2026-01-02 23:29:08 +0530

    Testing code for add to favourite btn click

--- src/tests/PropertyCard.test.jsx
+++ src/tests/PropertyCard.test.jsx
@@ -3,3 +3,22 @@ import { MemoryRouter } from 'react-router-dom';
 import PropertyCard from '../components/PropertyCard';
 import { vi, describe, it, expect } from 'vitest';
 
+describe('PropertyCard', () => {
+  //Test1
+  it("calls onFavourite function when Add to Favourite button is clicked", () => {
+    const testOnFavourite = vi.fn()
+    const property = { id: 1, title: 'Flat', price: 250000 }
+
+    render(
+      <MemoryRouter>
+        <PropertyCard p={property} onFavourite={testOnFavourite} />
+      </MemoryRouter>
+    )
+
+    const button = screen.getByRole('button', { name: /add to favourites/i })
+    fireEvent.click(button)
+
+    expect(testOnFavourite).toHaveBeenCalledTimes(1)
+    expect(testOnFavourite).toHaveBeenCalledWith(property)
+  });
+})
