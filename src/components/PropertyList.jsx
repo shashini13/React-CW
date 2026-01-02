2fbd52e2431f18efc585603f86b60d155b3d7c3a
Shashini <rshashini13@gmail.com>
2026-01-02 08:54:07 +0530

    Made new add-drag-drop div for mobile view only

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -1,22 +1,36 @@
 import PropertyCard from './PropertyCard';
 
-const PropertyList = ({results, onFavourite}) => {
+const PropertyList = ({
+    results,
+    onFavourite,
+    favouriteProperties,
+    handleAddDrop,
+    handleDragOver
+}) => {
     if (!results || results.length === 0) {
-        return <p class="property-list-div">No properties found.</p>
+        return <p className="property-list-div">No properties found.</p>
     };
 
     return (
+        
         <div className="property-list-div">
             <h2>Property List</h2>
-                <div className="property-list-content">
-                    {results.map((p) => (
+            <div
+                className="phone-view-add-to-fav"
+                onDrop={handleAddDrop}
+                onDragOver={handleDragOver}
+            >
+                <p>Drag here to add to favourites</p>
+            </div>
+            <div className="property-list-content">
+                {results.map((p) => (
                     <PropertyCard
                         key={p.id}
                         p = {p}
                         onFavourite={onFavourite}
                     />
-                    ))}
-                </div>
+                ))}
+            </div>
         </div>
     );
 }
